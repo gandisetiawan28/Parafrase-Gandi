0905f40f9aa4fb623d23abb1fe5f4fdae540374c
gandisetiawan28 <pro.gandisetiawan@gmail.com>
2026-04-22 21:15:33 +0700

    Fix React imports and bump version to 4.0.1

--- src/taskpane/components/App.jsx
+++ src/taskpane/components/App.jsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import { useState } from "react";
 import PropTypes from "prop-types";
 import { makeStyles, shorthands, TabList, Tab } from "@fluentui/react-components";
